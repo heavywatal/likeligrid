5f1adf003e6fb1f301cad1d463fdf2e26a72901a
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-09-20 17:57:45 +0900

    Improve genotype generator

--- generate.R
+++ generate.R
@@ -1,27 +1,72 @@
 #########1#########2#########3#########4#########5#########6#########7#########
-## Make dummy data
+## Functions
 
-n_obs = 4000L
-true_threshold = 0.5
-true_epsilon = 0.1
-true_coefs = c(RAS=0.15, TGF=0.30, Notch=0.05, Cycle=0.25, Damage=0.15)
-stopifnot(sum(true_coefs, true_epsilon) == 1.0)
-
-genotypes = rbernoulli(n_obs * length(true_coefs), 0.5) %>>%
+make_genotypes = function(n_obs, param_names, mu) {
+    rbernoulli(n_obs * length(param_names), mu) %>>%
     as.integer() %>>%
     matrix(nrow=n_obs) %>>%
-    {colnames(.) = names(true_coefs); .} %>>%
+    {colnames(.) = param_names; .}
+}
+
+add_interaction_term = function(.data, .indices) {
+    .names = names(.data)[.indices]
+    .eq = paste(.names, collapse='*')
+    names(.eq) = paste(.names, collapse=':')
+    dplyr::mutate_(.data, .dots=.eq)
+}
+# as_tibble(genotypes) %>>% add_interaction_term(c(1, 2))
+# as_tibble(genotypes) %>>% add_interaction_term(c(1, 2, 3))
+
+make_genotypes2 = function(genotypes) {
+    .dimensions = ncol(genotypes)
+    .combn = combn(.dimensions, 2)
+    .genotypes2 = as_tibble(genotypes)
+    for (i in seq_len(ncol(.combn))) {
+        .genotypes2 = add_interaction_term(.genotypes2, .combn[,i])
+    }
+    .genotypes2
+}
+
+make_phenotypes = function(genotypes, coefs, sd_coef, threshold, epsilon=0.1) {
+    genotypes %>>% {
+        .mu = . %*% coefs + epsilon
+        tibble::as_tibble(.) %>>%
+        dplyr::mutate(true_mu=.mu)
+    } %>>% dplyr::mutate(
+        true_sigma=sqrt(true_mu) * sd_coef,
+        true_score= rnorm(nrow(.), true_mu, true_sigma),
+        cancer= true_score >= threshold)
+}
+
+#########1#########2#########3#########4#########5#########6#########7#########
+## Make dummy data
+
+n_obs = 16000L
+mu = 0.05
+true_threshold = 0.25
+true_epsilon = 0.1
+sd_coef = 0.2
+true_coefs = c(0.15, 0.30, 0.05, 0.25, 0.15)
+names(true_coefs) = head(names(genotype_pereira), length(true_coefs))
+stopifnot(sum(true_coefs, true_epsilon) == 1.0)
+
+genotypes = make_genotypes(n_obs, names(true_coefs), mu) %>>%
     (? head(.))
 
-.data = genotypes %>>% {
-        .mu = . %*% true_coefs + true_epsilon
-        tibble::as_tibble(.) %>>%
-        dplyr::mutate(true_mu=.mu, true_sigma=sqrt(true_mu))
-    } %>>%
-    dplyr::mutate(
-        true_score= rnorm(nrow(.), true_mu, true_sigma),
-        cancer= true_score >= true_threshold) %>>%
+.data = make_phenotypes(genotypes, true_coefs, sd_coef, true_threshold, true_epsilon) %>>%
     (?.) %>>% (? dplyr::count(., cancer))
+
+.data %>>%
+    dplyr::select(-matches('^true_')) %>>%
+    dplyr::mutate(rm=rowMeans(dplyr::select(., -cancer))) %>>%
+    ggplot(aes(rm, fill=cancer))+
+    geom_histogram(position='dodge')+
+    wtl::theme_wtl()
+
+.data %>>%
+    ggplot(aes(true_mu))+
+    geom_histogram(aes(fill=cancer), bins=32, position='dodge')+
+    wtl::theme_wtl()
 
 .geno_matrices = .data %>>%
     dplyr::select(-matches('^true_')) %>>%
@@ -36,41 +81,21 @@ genotypes = rbernoulli(n_obs * length(true_coefs), 0.5) %>>%
 #########1#########2#########3#########4#########5#########6#########7#########
 # interaction terms
 
-add_interaction_term = function(.data, .indices) {
-    .names = names(.data)[.indices]
-    .eq = paste(.names, collapse='*')
-    names(.eq) = paste(.names, collapse=':')
-    dplyr::mutate_(.data, .dots=.eq)
-}
-as_tibble(genotypes) %>>% add_interaction_term(c(1, 2))
-as_tibble(genotypes) %>>% add_interaction_term(c(1, 2, 3))
-
-.dimensions = ncol(genotypes)
-.combn = combn(.dimensions, 2)
-.genotypes2 = as_tibble(genotypes)
-for (i in seq_len(ncol(.combn))) {
-    .genotypes2 = add_interaction_term(.genotypes2, .combn[,i])
-}
-.genotypes2
-true_coefs2 = .genotypes2 %>>%
+genotypes2 = make_genotypes2(genotypes) %>>% (?.)
+true_coefs2 = genotypes2 %>>%
     {setNames(runif(ncol(.)), names(.))} %>>%
     (. * (1.0 - true_epsilon) / sum(.)) %>>%
     (?.) %>>% (?sum(.))
 stopifnot(sum(true_coefs2, true_epsilon) == 1.0)
 
-.data2 = as.matrix(.genotypes2) %>>% {
-        .mu = . %*% true_coefs2 + true_epsilon
-        tibble::as_tibble(.) %>>%
-        dplyr::mutate(true_mu=.mu, true_sigma=sqrt(true_mu))
-    } %>>%
-    dplyr::mutate(
-        true_score= rnorm(nrow(.), true_mu, true_sigma),
-        cancer= true_score >= true_threshold) %>>%
+.data2 = make_phenotypes(as.matrix(genotypes2), true_coefs2, sd_coef, true_threshold, true_epsilon) %>>%
     (?.) %>>% (? dplyr::count(., cancer))
 
 .genotype_cancer2 = .data2 %>>%
     dplyr::filter(cancer) %>>%
     dplyr::select(-matches('^true_|^cancer$|:')) %>>% (?.)
+
+.genotype_cancer2 %>>% wtl::write_df('genotype2.tsv')
 
 for (i in seq_len(ncol(.combn))) {
     .df = add_interaction_term(.genotype_cancer2, .combn[,i])
@@ -78,6 +103,18 @@ for (i in seq_len(ncol(.combn))) {
     message(.term)
     wtl::write_df(.df, sprintf('genotype2_%s.tsv', .term))
 }
+
+#########1#########2#########3#########4#########5#########6#########7#########
+#
+
+genotype_pereira = readr::read_tsv('~/Dropbox/working/tumor/genotype_pereira+er.tsv')
+genotype_pereira %>>% dplyr::summarise_all(mean)
+genotype_pereira %>>% as.matrix() %>>% table()
+genotype_pereira %>>% dplyr::select(-Other) %>>% as.matrix() %>>% table()
+
+genotype_pereira %>>%
+    dplyr::mutate(rm=rowMeans(.)) %>>%
+    ggplot(aes(rm))+geom_histogram()+wtl::theme_wtl()
 
 #########1#########2#########3#########4#########5#########6#########7#########
 # Use only cancer data
