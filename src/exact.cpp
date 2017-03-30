cc632d8c2736b72994bd4b445818773c3fa4e19f
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-03-30 19:44:36 +0900

    Copy common part from the previous model

--- src/exact.cpp
+++ src/exact.cpp
@@ -9,14 +9,18 @@
 #include <json.hpp>
 
 #include <wtl/debug.hpp>
+#include <wtl/exception.hpp>
 #include <wtl/iostr.hpp>
 #include <wtl/zfstream.hpp>
-#include <wtl/itertools.hpp>
 #include <wtl/algorithm.hpp>
 #include <wtl/math.hpp>
+#include <wtl/os.hpp>
+#include <wtl/eigen.hpp>
 
 namespace likeligrid {
 
+const std::vector<double> ExactModel::STEPS_ = {0.4, 0.2, 0.1, 0.05, 0.02, 0.01};
+const std::vector<size_t> ExactModel::BREAKS_ = {5, 5, 5, 6, 5, 5};
 bool ExactModel::SIGINT_RAISED_ = false;
 
 ExactModel::ExactModel(const std::string& infile, const size_t max_sites) {HERE;
@@ -86,14 +90,86 @@ ExactModel::ExactModel(const std::string& infile, const size_t max_sites) {HERE;
     std::cerr << "a_pathway_: " << a_pathway_ << std::endl;
 }
 
-void ExactModel::run() {HERE;
-    calc_loglik({0.5, 0.5, 0.5, 0.5});
+inline std::vector<std::valarray<double>>
+make_vicinity(const std::valarray<double>& center, const size_t breaks, const double radius, const double max=2.0) {
+    std::vector<std::valarray<double>> axes;
+    axes.reserve(center.size());
+    for (const double x: center) {
+        Eigen::ArrayXd axis = Eigen::ArrayXd::LinSpaced(breaks, x + radius, x - radius);
+        axis = (axis * 100.0).round() / 100.0;  // grid precision = 0.01
+        axes.push_back(wtl::eigen::valarray(wtl::eigen::filter(axis, (0.0 < axis) * (axis < max))));
+    }
+    return axes;
 }
 
-double ExactModel::calc_loglik(const std::valarray<double>& params) const {
-    double loglik = (a_pathway_ * std::log(params)).sum();
-    calc_denom(params, 2);
-    return loglik += lnp_const_;
+std::vector<std::valarray<double>> ExactModel::init_axes(const std::string& infile) {HERE;
+    if (read_results(infile)) {
+        if (skip_ > 0) {
+            throw std::runtime_error("infile must be a complete result");
+        }
+        std::cerr << "mle_params_: " << mle_params_ << std::endl;
+        ++stage_;
+        return make_vicinity(mle_params_, BREAKS_.at(stage_), STEPS_.at(stage_), 2.0);
+    } else {
+        const double step = STEPS_[0];
+        const size_t breaks = BREAKS_[0];
+        Eigen::ArrayXd axis = Eigen::VectorXd::LinSpaced(breaks, 2.0, step).array();
+        return std::vector<std::valarray<double>>(names_.size(), wtl::eigen::valarray(axis));
+    }
+}
+
+void ExactModel::run(const std::string& infile) {HERE;
+    const auto axes = init_axes(infile);
+    if (stage_ == STEPS_.size()) {
+        return;
+    }
+    std::string outfile = "/dev/stdout";
+    if (infile != "/dev/null") {
+        auto oss = wtl::make_oss(2, std::ios::fixed);
+        oss << "grid-" << STEPS_.at(stage_) << ".tsv.gz";
+        outfile = oss.str();
+        read_results(outfile);
+    }
+    for (size_t j=0; j<names_.size(); ++j) {
+        std::cerr << names_[j] << ": " << axes[j] << std::endl;
+    }
+    run_impl(outfile, wtl::itertools::product(axes));
+    if (outfile != "/dev/stdout") {
+        run(outfile);
+    }
+}
+
+void ExactModel::run_impl(const std::string& outfile, wtl::itertools::Generator<std::valarray<double>>&& gen) const {HERE;
+    auto buffer = wtl::make_oss();
+    std::ios::openmode mode = std::ios::out;
+    if (wtl::exists(outfile)) {
+        if (skip_ == 0 && outfile != "/dev/stdout") {
+            std::cerr << outfile << " exists." << std::endl;
+            return;
+        }
+        mode |= std::ios::app;
+    } else {
+        buffer << "##max_count=" << gen.max_count() << "\n";
+        buffer << "##max_sites=" << nsam_with_s_.size() - 1 << "\n";
+        buffer << "##step=" << STEPS_.at(stage_) << "\n";
+        buffer << "loglik\t" << wtl::join(names_, "\t") << "\n";
+    }
+    std::cerr << "Writing: " << outfile << std::endl;
+    std::cerr << skip_ << " to " << gen.max_count() << std::endl;
+    wtl::ozfstream fout(outfile, mode);
+    for (const auto& th_path: gen(skip_)) {
+        buffer << calc_loglik(th_path) << "\t"
+               << wtl::str_join(th_path, "\t") << "\n";
+        if (gen.count() % 100 == 0) {  // snapshot for long run
+            std::cerr << "*" << std::flush;
+            fout << buffer.str();
+            fout.strict_sync();
+            buffer.str("");
+        }
+        if (SIGINT_RAISED_) {throw wtl::ExitSuccess("KeyboardInterrupt");}
+    }
+    std::cerr << "\n";
+    fout << buffer.str();
 }
 
 class Denoms {
@@ -107,11 +183,11 @@ class Denoms {
         th_path_(th_path),
         annot_(annot),
         max_sites_(max_sites),
-        denoms_(max_sites_ + 1, 0.0) {
-
+        denoms_(max_sites + 1)
+    {
         mutate(bits_t(w_gene.size(), 0), bits_t(annot.size(), 0), 1.0);
     }
-    const std::vector<double>& get() const {return denoms_;}
+    const std::valarray<double> log() const {return std::log(denoms_);}
 
   private:
     void mutate(const bits_t& genotype, const bits_t& pathtype, const double anc_p) {
@@ -155,14 +231,77 @@ class Denoms {
     const std::valarray<double>& th_path_;
     const std::vector<bits_t>& annot_;
     const size_t max_sites_;
-    std::vector<double> denoms_;
+    std::valarray<double> denoms_;
 };
 
-double ExactModel::calc_denom(const std::valarray<double>& th_path, const size_t s) const {
-    std::cout << th_path << std::endl;
-    double sum_prob = 0.0;
-    std::cout << "D_s: " << Denoms(w_gene_, th_path, annot_, s).get() << std::endl;
-    return sum_prob;
+double ExactModel::calc_loglik(const std::valarray<double>& th_path) const {
+    const size_t max_sites = nsam_with_s_.size();
+    double loglik = (a_pathway_ * std::log(th_path)).sum();
+    const auto lnD = Denoms(w_gene_, th_path, annot_, max_sites).log();
+    // std::cout << "lnD: " << lnD << std::endl;
+    for (size_t s=2; s<max_sites; ++s) {
+        loglik -= nsam_with_s_[s] * lnD[s];
+    }
+    return loglik += lnp_const_;
+}
+
+bool ExactModel::read_results(const std::string& infile) {HERE;
+    if (infile == "/dev/null")
+        return false;
+    try {
+        wtl::izfstream ist(infile);
+        std::cerr << "Reading: " << infile << std::endl;
+        const size_t max_count = read_metadata(ist);
+        skip_ = read_body(ist);
+        if (skip_ == max_count) skip_ = 0;
+        return true;
+    } catch (std::ios::failure& e) {
+        if (errno != 2) throw;
+        return false;
+    }
+}
+
+size_t ExactModel::read_metadata(std::istream& ist) {HERE;
+    std::string buffer;
+    std::getline(ist, buffer);
+    const size_t max_count = std::stoul(wtl::split(buffer, "=")[1]);
+    std::getline(ist, buffer);
+    const size_t max_sites = std::stoul(wtl::split(buffer, "=")[1]);
+    if (nsam_with_s_.size() - 1 != max_sites) {
+        std::cerr << "Note: -s is different between arg and file\n";
+    }
+    std::getline(ist, buffer);
+    const double step = std::stod(wtl::split(buffer, "=")[1]);
+    auto pred = std::bind(wtl::approx<double>, std::placeholders::_1, step);
+    const auto it = std::find_if(STEPS_.begin(), STEPS_.end(), pred);
+    if (it == STEPS_.end()) throw std::runtime_error("invalid step size");
+    stage_ = it - STEPS_.begin();
+    return max_count;
+}
+
+size_t ExactModel::read_body(std::istream& ist) {HERE;
+    std::string buffer;
+    ist >> buffer; // loglik
+    std::getline(ist, buffer); // header
+    buffer.erase(0, 1); // \t
+    if (names_ != wtl::split(buffer, "\t")) {
+        std::cerr << names_ << std::endl;
+        std::cerr << wtl::split(buffer, "\t") << std::endl;
+        throw std::runtime_error("Column names are wrong");
+    }
+    size_t i = 0;
+    double max_ll = std::numeric_limits<double>::lowest();
+    while (std::getline(ist, buffer)) {
+        ++i;
+        std::istringstream iss(buffer);
+        std::istream_iterator<double> it(iss);
+        if (*it > max_ll) {
+            max_ll = *it;
+            std::vector<double> v(++it, std::istream_iterator<double>());
+            mle_params_ = std::valarray<double>(v.data(), v.size());
+        }
+    }
+    return i;
 }
 
 void ExactModel::unit_test() {HERE;
--- src/exact.hpp
+++ src/exact.hpp
@@ -12,6 +12,7 @@
 #include <valarray>
 
 #include <boost/dynamic_bitset.hpp>
+#include <wtl/itertools.hpp>
 
 namespace likeligrid {
 
@@ -20,21 +21,27 @@ typedef boost::dynamic_bitset<> bits_t;
 
 class ExactModel {
   public:
+    static const std::vector<double> STEPS_;
+    static const std::vector<size_t> BREAKS_;
+
     ExactModel() = default;
     ExactModel(
         const std::string& infile,
         const size_t max_sites=-1);
-    void run();
+    void run(const std::string& infile="");
 
     static void raise_sigint() {SIGINT_RAISED_ = true;}
     static void unit_test();
 
     /////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
   private:
-    double calc_loglik(const std::valarray<double>& params) const;
-    double calc_denom(
-        const std::valarray<double>& params,
-        const size_t num_mutations) const;
+    std::vector<std::valarray<double>> init_axes(const std::string& infile);
+    void run_impl(const std::string& outfile, wtl::itertools::Generator<std::valarray<double>>&&) const;
+    double calc_loglik(const std::valarray<double>& th_path) const;
+
+    bool read_results(const std::string&);
+    size_t read_metadata(std::istream&);
+    size_t read_body(std::istream&);
 
     std::vector<std::string> names_;
     std::vector<bits_t> annot_;
@@ -43,7 +50,10 @@ class ExactModel {
     std::valarray<double> a_pathway_;
     std::vector<size_t> nsam_with_s_;
     double lnp_const_ = 0.0;
+
     std::valarray<double> mle_params_;
+    size_t skip_ = 0;
+    size_t stage_ = 0;
 
     static bool SIGINT_RAISED_;
 };
