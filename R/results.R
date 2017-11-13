0783ab6f78c9a53302db17b8b1a88be2dd8d94f7
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-11-13 19:25:46 +0900

    Summarize likelihood elevation in gradient mode

--- R/results.R
+++ R/results.R
@@ -9,6 +9,8 @@ source('~/git/likeligrid/R/common.R')
 
 .metadata = read_metadata(.datadir) %>% print()
 
+# #######1#########2#########3#########4#########5#########6#########7#########
+
 .results = .metadata %>%
     dplyr::filter(is.na(epistasis)) %>%
     dplyr::filter(s == 4L) %>%
@@ -63,3 +65,56 @@ ggsave('gradient5.pdf', .p, width=12, height=9)
 .pl = .plts_epistasis %>% purrr::map(~{cowplot::plot_grid(plotlist=.x)})
 # ggsave('epistasis-s4.pdf', .pl, width=20, height=20)
 ggsave('epistasis-s5.pdf', .pl, width=20, height=20)
+
+# #######1#########2#########3#########4#########5#########6#########7#########
+# summarize likelihood elevation in gradient mode
+
+.results_ge = .metadata %>%
+    dplyr::filter(s == 4L) %>%
+    dplyr::filter(is.na(epistasis)) %>%
+    # head(3L) %>%
+    dplyr::mutate(
+      gradfiles = purrr::map(indir, ~list.files(.x, 'gradient-s5-e.*\\.gz$'))
+    ) %>%
+    tidyr::unnest() %>%
+    dplyr::mutate(
+      epistasis= str_extract(gradfiles, '(?<=-e)(\\dx\\d)'),
+      grad_epi= purrr::map(file.path(indir, gradfiles), ~{
+        message(.x)
+        .df = read_likeligrid(.x) #%>% dplyr::arrange(loglik)
+        .epistasis_col = .df %>% dplyr::select(ncol(.))
+        .start = .df %>% dplyr::filter(.epistasis_col == 1.0) %>% top_n(1L, loglik)
+        .end = .df %>% top_n(1L, loglik)
+        dplyr::bind_rows(.start, .end) %>%
+        tidyr::gather(pathway, value, -loglik)
+      }),
+      gradfiles= NULL
+    ) %>%
+    tidyr::unnest() %>%
+    dplyr::mutate(pathway= str_replace(pathway, '\\w+:\\w+', 'epistasis')) %>%
+    print()
+
+.nested = .results_ge %>%
+    dplyr::select(type, epistasis, loglik, pathway, value) %>%
+    tidyr::nest(-type) %>%
+    print()
+
+.plot = function(.tbl, .title='') {
+    .start_loglik = min(.tbl$loglik)
+    ggplot(.tbl, aes(value, loglik))+
+    geom_hline(data=.const.chisq, aes(yintercept=.start_loglik + y, colour=alpha))+
+    geom_path(size=1.3)+
+    facet_grid(epistasis ~ pathway)+
+    coord_cartesian(xlim=c(0, 2))+
+    scale_x_continuous(breaks=c(0, 1, 2))+
+    labs(title=.title)+
+    theme_wtl()+
+    theme(legend.position='none')
+}
+.plot(.nested$data[[1]], 'test')
+
+.plt = .nested %>%
+    dplyr::mutate(plt= purrr::map2(data, type, .plot)) %>%
+    dplyr::pull(plt)
+
+ggsave('gradient_epistasis-s5.pdf', .plt, width=7, height=9.9)
