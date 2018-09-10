2a2ecff15e757357cb49f79e04a48afe3e3dc333
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-10 21:24:13 +0900

    :art: Exit gracefully

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
     @brief Only defines tiny main()
 */
 #include "src/program.hpp"
+#include <iostream>
+#include <stdexcept>
 
 //! Just instantiate and run Program
 int main(int argc, char* argv[]) {
     std::vector<std::string> arguments(argv, argv + argc);
-    likeligrid::Program program(arguments);
-    program.run();
+    try {
+        likeligrid::Program program(arguments);
+        program.run();
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
