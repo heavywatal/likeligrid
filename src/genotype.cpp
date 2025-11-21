e95eecde1227ff61167b429c71d70a1a3c13b7f6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-11-21 14:01:06 +0900

    build: Follow cxxwtl 0.11.2

--- src/genotype.cpp
+++ src/genotype.cpp
@@ -173,7 +173,7 @@ void GenotypeModel::benchmark(const int n) {
     std::cerr << "width: " << num_genes_ << std::endl;
     std::cerr << "depth: " << max_sites_ << std::endl;
     std::cerr << "w ^ d: " << leaves * 1e-6 << " M" <<std::endl;
-    std::cerr << wtl::diff_rusage([&param,this]() {calc_loglik(param);}, n);
+    std::cerr << wtl::delta_rusage([&param,this]() {calc_loglik(param);}, n);
 }
 
 } // namespace likeligrid
