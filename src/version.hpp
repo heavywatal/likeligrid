3248ad1f77557446f168838ea839c070d9a51ea5
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-01-05 16:05:17 +0900

    fix: Generate version.cpp instead of version.hpp

---
+++ src/version.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef LIKELIGRID_VERSION_HPP_
+#define LIKELIGRID_VERSION_HPP_
+
+namespace likeligrid {
+
+extern const char* const PROJECT_NAME;
+extern const char* const PROJECT_VERSION;
+
+}
+
+#endif // LIKELIGRID_VERSION_HPP_
